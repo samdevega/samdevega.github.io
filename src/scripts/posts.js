6cb5ec176f7d367d34193b2ce2892b2610393155
root <root@DESKTOP-VQDM5KL.localdomain>
2023-10-13 12:06:48 +0100

    continuous deployment

--- src/scripts/posts.js
+++ src/scripts/posts.js
@@ -7,9 +7,8 @@ export const translatePosts = async (lang) => {
   const postCollection = await getCollection('posts') || []
   const sortedPosts = postCollection.sort((a, b) => +(b.data.pubDate.toISOString().split('T')[0].replaceAll('-', '')) - +(a.data.pubDate.toISOString().split('T')[0].replaceAll('-', '')))
   return sortedPosts.filter(post => post.slug.includes(`${lang}/`)).map((post, index, posts) => {
-    post.data.next = posts[index - 1]?.slug
+    post.data.next = posts[index - 1]?.slug.slice(3)
     post.data.previous = posts[index + 1]?.slug.slice(3)
-    post.slug = post.slug.slice(3)
     return post
   })
 }
