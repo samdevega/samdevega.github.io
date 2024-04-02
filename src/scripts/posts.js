e8e3083725f484e0d04f0db67e8ae6a9216f5ad8
samdevega <s.a.devega.garcia@gmail.com>
2024-04-03 00:53:23 +0100

    feature. estimated post reading time

--- src/scripts/posts.js
+++ src/scripts/posts.js
@@ -11,6 +11,7 @@ export const translatePosts = async (lang) => {
   const postCollection = await getCollection('posts') || []
   const sortedPosts = postCollection.sort((a, b) => postPubDateToInt(b) - postPubDateToInt(a))
   return sortedPosts.filter(post => post.slug.includes(`${lang}/`)).map((post, index, posts) => {
+    post.data.minRead = Math.ceil(post.body.replaceAll(/<table>(.|\n)*<\/table>/g, '').split(' ').length / 200)
     post.data.next = posts[index - 1]?.slug.slice(3)
     post.data.previous = posts[index + 1]?.slug.slice(3)
     return post
