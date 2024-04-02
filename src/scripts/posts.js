d38535f3730f4407928726c29626edda7a209279
samdevega <s.a.devega.garcia@gmail.com>
2024-04-02 23:15:07 +0100

    refactoring. extract function. remove duplication

--- src/scripts/posts.js
+++ src/scripts/posts.js
@@ -1,11 +1,15 @@
 import { getCollection } from 'astro:content'
 
+function postPubDateToInt(post) {
+  return +(post.data.pubDate.toISOString().split('T')[0].replaceAll('-', ''))
+}
+
 export const posts = await getCollection('posts') || []
-posts.sort((a, b) => +(b.data.pubDate.toISOString().split('T')[0].replaceAll('-', '')) - +(a.data.pubDate.toISOString().split('T')[0].replaceAll('-', '')))
+posts.sort((a, b) => postPubDateToInt(b) - postPubDateToInt(a))
 
 export const translatePosts = async (lang) => {
   const postCollection = await getCollection('posts') || []
-  const sortedPosts = postCollection.sort((a, b) => +(b.data.pubDate.toISOString().split('T')[0].replaceAll('-', '')) - +(a.data.pubDate.toISOString().split('T')[0].replaceAll('-', '')))
+  const sortedPosts = postCollection.sort((a, b) => postPubDateToInt(b) - postPubDateToInt(a))
   return sortedPosts.filter(post => post.slug.includes(`${lang}/`)).map((post, index, posts) => {
     post.data.next = posts[index - 1]?.slug.slice(3)
     post.data.previous = posts[index + 1]?.slug.slice(3)
