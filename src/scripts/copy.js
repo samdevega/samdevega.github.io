18394efd98270617e20a8f1e2e771be3aad189d3
samdevega <s.a.devega.garcia@gmail.com>
2025-08-10 14:12:09 +0100

    feature. embed text title into code block header with the copy button

--- src/scripts/copy.js
+++ src/scripts/copy.js
@@ -5,26 +5,34 @@ const t = useTranslations(lang)
 const codeNodes = document.querySelectorAll('pre:not([data-language="plaintext"])>code')
 
 codeNodes.forEach(node => {
-  let timeoutId;
-  const preHeader = document.createElement('div');
+  let timeoutId
+  const preHeader = document.createElement('div')
+  const previousDomNode = node.parentElement.previousElementSibling
+  const isPreTitle = previousDomNode
+    && previousDomNode.tagName === 'P'
+    && previousDomNode.children.length === 1
+    && previousDomNode.querySelector('strong')
+  if (isPreTitle) {
+    preHeader.appendChild(previousDomNode)
+  }
   const button = document.createElement('button')
-  preHeader.className = 'pre-header';
+  preHeader.className = 'pre-header'
   button.className = 'copy-button'
   button.innerText = t('copy')
   button.onclick = () => {
-    navigator.clipboard.writeText(node.innerText);
-    button.toggleAttribute('disabled');
-    button.innerText = t('copied');
+    navigator.clipboard.writeText(node.innerText)
+    button.toggleAttribute('disabled')
+    button.innerText = t('copied')
 
     if (timeoutId) {
-      clearTimeout(timeoutId);
+      clearTimeout(timeoutId)
     }
 
     timeoutId = setTimeout(() => {
-      button.innerText = t('copy');
-      button.toggleAttribute('disabled');
-      timeoutId = null;
-    }, 2000);
+      button.innerText = t('copy')
+      button.toggleAttribute('disabled')
+      timeoutId = null
+    }, 2000)
   }
   preHeader.appendChild(button)
   node.parentNode.parentNode.insertBefore(preHeader, node.parentNode)
